8feb4707b9c6d2149412d43b3293ca74f33e75ec
Quyentuy2004 <quyentuybinhchinh26082004@gmail.com>
2025-05-14 16:55:09 +0700

    Add files via upload

--- SETUP_WIFI_ESP32.ino
+++ SETUP_WIFI_ESP32.ino
@@ -1,18 +1,16 @@
+#include <dummy.h>
+
 #include "wifiConfic.h"
 #include <Arduino.h>
-#include "dodht11.h"
+
 
 void setup() {
   Serial.begin(115200);
   wifiConfig.begin();
-   // Cấu hình Web Server web 1
-  if(WiFi.status()==WL_CONNECTED){
-    dhtConfig.begin();
-  }
+  
+  
 }
 void loop() {
   wifiConfig.run();
-  if(WiFi.status()==WL_CONNECTED){
-   dhtConfig.run();
-  }
+  
 }
